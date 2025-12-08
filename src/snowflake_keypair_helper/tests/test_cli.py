95ff9bea11054715f17a64926414c85518b9955a
dlovell <dlovell@gmail.com>
2025-12-08 15:11:13 -0500

    test(cli): add tests of skh-assign-public-key

--- src/snowflake_keypair_helper/tests/test_cli.py
+++ src/snowflake_keypair_helper/tests/test_cli.py
@@ -1,14 +1,17 @@
-import pytest
+import re
 from subprocess import (
     Popen,
     PIPE,
 )
+
+import pytest
 
 from snowflake_keypair_helper.cli import (
     gen_commands,
 )
 from snowflake_keypair_helper.constants import (
     snowflake_env_var_prefix,
+    gh_test_user,
 )
 from snowflake_keypair_helper.env_utils import (
     parse_env_path,
@@ -75,3 +78,33 @@ def test_cli_list_cli_commands(tmp_path, monkeypatch):
     assert not err
     assert not tuple(tmp_path.iterdir())
     assert all(command.name in out for command in gen_commands())
+
+
+@pytest.mark.benchmark
+def test_assign_public_key_no_args():
+    command = "skh-assign-public-key"
+    returncode, _, err, _ = do_popen_communicate(command, gh_test_user)
+    assert returncode != 0
+    infix = "one and only one value must be non None"
+    assert re.match(f".*{infix}.*", err, re.DOTALL), err
+
+
+@pytest.mark.parametrize("args", (("--oneline",), ("--no-oneline",), ()))
+def test_assign_public_key_from_path(args, tmp_path, monkeypatch):
+    # writes to stdout, not cwd/stderr
+    monkeypatch.chdir(tmp_path)
+    name = "-"
+    (returncode, out, err, _) = do_popen_communicate(
+        "skh-generate-keypair", name, *args
+    )
+    assert not returncode
+    assert not err
+    assert not tuple(tmp_path.iterdir())
+    path = tmp_path.joinpath("my.env")
+    path.write_text(out)
+
+    command = "skh-assign-public-key"
+    returncode, out, err, _ = do_popen_communicate(
+        command, gh_test_user, "--path", str(path)
+    )
+    assert returncode == 0
